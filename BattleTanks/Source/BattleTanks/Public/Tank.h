1f2a0001cf51a6b79a04e7cd1c307f07271f653e
Kyle <kylehatfield1@gmail.com>
2016-11-13 22:07:36 -0600

    BlueprintPure and HealthBars

--- BattleTanks/Source/BattleTanks/Public/Tank.h
+++ BattleTanks/Source/BattleTanks/Public/Tank.h
@@ -12,13 +12,17 @@ class BATTLETANKS_API ATank : public APawn
 {
 	GENERATED_BODY()
 
-	// Sets default values for this pawn's properties
-	ATank();
-
 public:
 	virtual float TakeDamage(float DamageAmount, struct FDamageEvent const & DamageEvent, class AController * EventInstigator, AActor * DamageCauser) override;
 
+	UFUNCTION(BlueprintPure, Category = "Health")
+	float GetHealthPercent() const;
+
 private:
+
+	// Sets default values for this pawn's properties
+	ATank();
+
 	UPROPERTY(EditDefaultsOnly, Category = "Setup")
 	int32 StartingHealth = 100;
 
